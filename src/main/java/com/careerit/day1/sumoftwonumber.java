b2ecb53114674e4d9dfe22977c446359cc3780ed
Rezan1609 <riza960916@gmail.com>
2023-04-27 14:24:13 -0400

    First Commit

---
+++ src/main/java/com/careerit/day1/sumoftwonumber.java
@@ -0,0 +1,11 @@
+package com.careerit.day1;
+
+public class sumoftwonumber {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        System.out.println(a+"+"+b+"="+sum);
+    }
+}
