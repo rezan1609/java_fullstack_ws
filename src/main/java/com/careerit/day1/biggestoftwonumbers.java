288d4e9967411328ea028598ffcc8dcde3a7161c
Rezan1609 <riza960916@gmail.com>
2023-04-27 14:28:18 -0400

    biggestoftwonumbers

---
+++ src/main/java/com/careerit/day1/biggestoftwonumbers.java
@@ -0,0 +1,9 @@
+package com.careerit.day1;
+
+public class biggestoftwonumbers {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("Biggest is :"+(a>b ? a:b));
+    }
+}
